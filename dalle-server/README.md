9dd52eb785085d8759ff75ef8ebc5c67dd409fb4
minwook <minkj1992@gmail.com>
2023-04-22 19:21:24 +0900

    feat: dalle image generator by text

---
+++ dalle-server/README.md
@@ -0,0 +1,14 @@
+# Dalle image generator
+
+
+
+```sh
+cp .env.example .env
+# fill openai_api_key
+
+make run
+
+
+# query = Mit bitcoin hackton rest in peace
+http://127.0.0.1:8000/img/?desc=Mit%20bitcoin%20hackton%20rest%20in%20peace
+```
--- dalle-server/src/main.py
+++ dalle-server/src/main.py
@@ -1,16 +1,18 @@
 import logging
 
+import openai
 import uvicorn
-from fastapi import FastAPI, Request, status
+from dotenv import dotenv_values
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from openai.openai_object import OpenAIObject
 
-from config import get_config
+cfg = dotenv_values('.env')
 
-logger = logging.getLogger(__name__)
 
-cfg = get_config()
+
 app = FastAPI(title='Dalle image api server')
-# Your CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,6 +27,22 @@ async def pong():
     return {"ping": "pong!"}
 
 
+@app.get("/img/")
+async def get_img(desc: str):
+    b64_json = await get_a_img_from_openai(desc)
+    return JSONResponse(content=b64_json)
+
+
+async def get_a_img_from_openai(desc:str):
+    openai_obj:OpenAIObject = await openai.Image.acreate(
+            api_key=cfg['OPENAI_API_KEY'],
+            prompt=desc,
+            n=1,
+            size="512x512",
+            response_format="b64_json",
+        )
+
+    return openai_obj['data'][0]
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("src.main:app", host="127.0.0.1", port=8080, reload=True)
